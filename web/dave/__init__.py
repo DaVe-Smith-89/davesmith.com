44740e238f0360254fcf4e922ed3613c080ae00a
Dave Smith <73696370+DaVe-Smith-89@users.noreply.github.com>
2021-10-23 05:05:16 -0700

    Update __init__.py

--- web/dave/__init__.py
+++ web/dave/__init__.py
@@ -6,7 +6,7 @@ from flask_admin import Admin
 app = Flask(__name__)
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
-admin = Admin(app, url='/3xf8z81HUddaTkyqZBXJm9PGpakaya')
+admin = Admin(app, url='/3xf8z81HUddaTkyqZBXJm9PG')
 
 app.config['SECRET_KEY'] = '49d373ffd06031f5bb8b3d54823ab22b1d630e89d6dab19fd320c3640408ed43'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://uk7ffmkxooxbon3o:N9JWxqwP8rZiddZRkNC6@bodlbp3olvcjoewvmkih-mysql.services.clever-cloud.com:3306/bodlbp3olvcjoewvmkih'
